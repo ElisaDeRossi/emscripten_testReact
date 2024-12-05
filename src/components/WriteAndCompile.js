14a0f914c35a9b4b522df928de4b78120e7e4d64
ElisaDeRossi <76956698+ElisaDeRossi@users.noreply.github.com>
2024-12-05 17:49:48 +0100

    Override output and compile to js code

--- src/components/WriteAndCompile.js
+++ src/components/WriteAndCompile.js
@@ -1,93 +1,6 @@
 import React, { useState } from "react";
 import Emscriptenjs from "@emscriptenjs/emscriptenjs"
 
-export const spinner = (size) => `
-    <div style="font-size: calc(${size}px / 13)">
-        <div style="width: 13em;height: 13em;overflow: hidden;align-items: center;justify-items: center;display: grid;padding: 2em;">
-            <style>
-                .loader,
-                .loader:after {
-                    border-radius: 50%;
-                    width: 10em;
-                    height: 10em;
-                }
-                .loader {
-                    margin: 0;
-                    position: relative;
-                    text-indent: -9999em;
-                    border-top: 1.1em solid rgba(51, 102, 153, 0.2);
-                    border-right: 1.1em solid rgba(51, 102, 153, 0.2);
-                    border-bottom: 1.1em solid rgba(51, 102, 153, 0.2);
-                    border-left: 1.1em solid #336699;
-                    -webkit-transform: translateZ(0);
-                    -ms-transform: translateZ(0);
-                    transform: translateZ(0);
-                    -webkit-animation: load8 1.1s infinite linear;
-                    animation: load8 1.1s infinite linear;
-                }
-                @-webkit-keyframes load8 {
-                    0% {
-                        -webkit-transform: rotate(0deg);
-                        transform: rotate(0deg);
-                    }
-                    100% {
-                        -webkit-transform: rotate(360deg);
-                        transform: rotate(360deg);
-                    }
-                }
-                @keyframes load8 {
-                    0% {
-                        -webkit-transform: rotate(0deg);
-                        transform: rotate(0deg);
-                    }
-                    100% {
-                        -webkit-transform: rotate(360deg);
-                        transform: rotate(360deg);
-                    }
-                }
-            </style>
-            <div class="loader"></div>
-        </div>
-    </div>
-`;
-
-export const previewTemplate = (icon, title, message) => `
-    <html>
-        <head>
-            <title>Emscriptenjs output preview</title>
-            <style>
-                html, body {
-                    display: grid;
-                    grid-template-rows: 1fr 0;
-                    grid-template-columns: 1fr 0;
-                    align-items: center;
-                    justify-items: center;
-                }
-                #title,
-                #message {
-                    font-family: Roboto, sans-serif;
-                    margin: 0;
-                    padding: 0;
-                }
-                #icon,
-                #title,
-                #message {
-                    display: grid;
-                    align-items: center;
-                    justify-items: center;
-                }
-            </style>
-        </head>
-        <body>
-            <div id="container">
-                <h1 id="title">${title}</h1>
-                <div id="icon">${icon}</div>
-                <div id="message">${message}</div>
-            </div>
-        </body>
-    </html>
-`;
-
 const defaultPrograms = [
 `#include <iostream>
 int main(){
@@ -167,50 +80,55 @@ function WriteAndCompile() {
         margin: '5px'
     };
 
-    const [codeString, setCodeString] = useState(defaultPrograms[2]);
+    const [codeString, setCodeString] = useState(defaultPrograms[1]);
     let [output, setOutput] = useState([]);
-    
+
     const compile = async () => {
+
+        let initialOutput = [];
+        
+        // Override prompt function
+        /* TODO: doesn't work for some reason
+        const originalPrompt = window.prompt;
+        window.prompt = (message, def) => {
+            initialOutput.push("> " + message + ": ");
+            setOutput([...initialOutput]);
+            // originalPrompt(message, def);
+        };
+        */
+
+        // Override console.log function
+        const originalLog = console.log;
+        console.log = (message) => {
+            initialOutput.push(message); 
+            setOutput([...initialOutput]);
+        };
+
+        console.log("> Initiating emscriptenjs");
         const emscriptenjs = new Emscriptenjs();
         await emscriptenjs.init();
         await emscriptenjs.fileSystem.writeFile("/working/main.cpp", codeString);
         
-        console.log("Compiling code");
+        console.log("> Compiling code");
         const cmd = "em++ -O2 -fexceptions -sEXIT_RUNTIME=1 -sSINGLE_FILE=1 -sMINIFY_HTML=0 -sUSE_CLOSURE_COMPILER=0 main.cpp -o main.js";
         const result = await emscriptenjs.run(cmd);
-
         
         if (result.returncode === 0) {
-            console.log("Compilation was successful");
-            const content = await emscriptenjs.fileSystem.readFile("/working/main.js", { encoding: "utf8" });
+            console.log("> Compilation was successful");
+            console.log("> OUTPUT: ");
 
+            const content = await emscriptenjs.fileSystem.readFile("/working/main.js", { encoding: "utf8" });
+            // eslint-disable-next-line
             const funct = new Function(content);
 
-            // Override console.log function
-            const originalLog = console.log;
-            output = [];
-            console.log = (message) => {
-                output.push(message); 
-                setOutput(output);
-            };
-
-            // TODO: Override window.prompt function
-            /*
-            const originalPrompt = window.prompt;
-            window.prompt = (message, defaultInput) => {
-                originalLog("aaaaaaaaaaaaaaaaaa");
-                // return originalPrompt(message, defaultInput);
-            };
-            */
-
             funct();
-
-            // Reassign functions
-            console.log = originalLog;
-            // window.prompt = originalPrompt;
         } else {
             alert("Compilation failed");
         }
+
+        // Reassign functions
+        console.log = originalLog;
+        // window.prompt = originalPrompt;
     };
 
     return (
